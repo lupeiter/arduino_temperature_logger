522b28a100b2095172be7b3dd863b2f031fb4ded
lupeiter <lupeiter@hotmail.com>
2024-03-20 15:44:07 +0100

    Add and initialize OneWire library

--- temperature_logger_code/temperature_logger_code.ino
+++ temperature_logger_code/temperature_logger_code.ino
@@ -1,6 +1,10 @@
 /*Arduino temperature logger
-Using DS18820 sensor
+Using DS18B20 sensor
 */
+#include OneWire.h
+
+#define DS18B20_Pin 8
+OneWire ow(DS18B20_Pin);
 
 void setup(){
 
@@ -8,6 +12,10 @@ void setup(){
 }
 
 void loop(){
+// 1st sequence: Initialization
 
+// 2nd sequence: ROM command
+
+// 3rd sequence: DS18B20 Function Command
 
 }
