73b88c44f60f55acedd9d177aa470f764db9eea1
lupeiter <lupeiter@hotmail.com>
2024-03-20 16:07:17 +0100

    Add sequence to read ROM code

--- temperature_logger_code/temperature_logger_code.ino
+++ temperature_logger_code/temperature_logger_code.ino
@@ -8,14 +8,22 @@ OneWire ow(DS18B20_Pin);
 
 void setup(){
 
-
 }
 
 void loop(){
-// 1st sequence: Initialization
+    /* We always need to follow this 1, 2 and 3 sequence */
+    byte rom_code(8); // create an array containing 8 elements of type byte for the rom code
+  
+    ow.reset(); // 1st step: Initialization
 
-// 2nd sequence: ROM command
+    // 2nd step: ROM command (since we only have one slave, we use Read Room 33). This reads the ROM information.
+    ow.write(0x33);
 
-// 3rd sequence: DS18B20 Function Command
+    for (int i=0; i<8; i++){
+        rom_code[i] = ow.read();
+    }
+
+    // 3rd step: DS18B20 Function Command. Start sequence to convert temperature.
+
 
 }
